89dc9d01b37075a604f256e863982d50226fa389
wskqukq511 <15321733@qq.com>
2017-06-08 17:27:38 +0800

    Default

--- sse/sse.py
+++ sse/sse.py
@@ -1,6 +1,7 @@
 from urllib import request
 import time
 import os
+import codecs
 import csv
 
 filename = time.strftime("%Y%m%d")+".js"
@@ -13,15 +14,35 @@ else:
 
 all_the = open(filename,encoding='utf-8')
 #print(all_the)
-for line in all_the:
-    data = line[line.find('bulletin_date:"'):line.find('",bulletin_year')][15:]
-    title = line[line.find('bulletin_title:"'):line.find('",bulletin_file_url')][16:]
-    gonggao_url = 'http://static.sse.com.cn/'+ line[line.find('bulletin_file_url:"'):line.find('",bulletin_time')][19:]
-    #print(type(data),type(title) ,type(gonggao_url))
-    csvfile = open(time.strftime("%Y%m%d")+'.csv','w')
-    writer = csv.writer(csvfile)
-    for key in data:
-        writer.writerow([key,data + ',' + title + ',' + gonggao_url])
+
+#返回公告连接和标题
+def getdata():
+    list_date = []
+    for line in all_the:
+        data = line[line.find('bulletin_date:"'):line.find('",bulletin_year')][15:]
+        title = line[line.find('bulletin_title:"'):line.find('",bulletin_file_url')][16:]
+        gonggao_url = 'http://static.sse.com.cn/' + line[line.find('bulletin_file_url:"'):line.find('",bulletin_time')][
+                                                    19:]
+        new_url = '<embed src="'+gonggao_url + '" width="800" height="600" />'
+        if data :
+            list_date.append([data, title, gonggao_url,new_url])
+
+
+        #print(list_date[0:3])
+    return list_date
+
+
+
+#print(getdata())
+#写入csv文件
+with open(time.strftime("%Y%m%d")+'.csv','wb') as f:
+    for item in getdata():
+        line = ','.join(item)+'\n'
+        f.write(line.encode('gb18030'))
+
+
+
+
 
 
 
